deba1e3d0be0305c9ef7a56124b8da2bf4252ea3
exp <f006g@qq.com>
2018-03-25 21:24:17 +0800

    A

---
+++ app/src/main/java/com/example/exp/gosling/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.exp.gosling;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+    }
+
+
+
+    }
+
