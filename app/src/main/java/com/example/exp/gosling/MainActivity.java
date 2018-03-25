5e8915ac59426070aca2f798ea7eab29448a1837
exp <f006g@qq.com>
2018-03-25 22:16:09 +0800

    AA

--- app/src/main/java/com/example/exp/gosling/MainActivity.java
+++ app/src/main/java/com/example/exp/gosling/MainActivity.java
@@ -1,20 +1,25 @@
 package com.example.exp.gosling;
 
 import android.content.res.Configuration;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
       setContentView(R.layout.activity_main);
     }
 
 
 
-    }
+}
 
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 #GoslingLovelace Basic Application
 
 ![Demo](display/gif3.gif)
+
+#the part2
+![](display/gif4.gif)
